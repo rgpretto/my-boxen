dcf7fb0e288231fb32460fea0099eacc50dcb883
Renzo G. Pretto <prettore@gmail.com>
2014-04-06 14:36:51 +0200

    Setup application configuration for user rgpretto

---
+++ modules/people/manifests/rgpretto.pp
@@ -0,0 +1,7 @@
+class people::rgpretto {
+  include people::rgpretto::applications
+#  include people::rgpretto::dotfiles
+#  include people::rgpretto::homebrew
+#  include people::rgpretto::osx_settings
+#  include people::rgpretto::projects
+}
---
+++ modules/people/manifests/rgpretto/applications.pp
@@ -0,0 +1,22 @@
+class people::rgpretto::applications {
+
+  include dropbox
+  include evernote
+  include github_for_mac
+  include gitx::dev
+  include googledrive
+  include iterm2::dev
+  # include iterm2::colors::solarized_light
+  # include iterm2::colors::solarized_dark
+  include keepassx
+  include libreoffice
+  include skype
+  include spotify
+  # include textwrangler # --- broken ---
+  include virtualbox
+  include vlc
+  
+#  pckeyboardhack::bind { 'remap capslock to ESC':
+#    mappings => { 'capslock' => 53 }
+#  }
+}
